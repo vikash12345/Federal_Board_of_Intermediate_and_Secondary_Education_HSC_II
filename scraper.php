b83bd416d0be66baa873769c731ddf87f7f04c62
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-04-30 09:15:17 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -14,7 +14,7 @@ Expert in web Scraping,Web automation,Investigation research and databases,
 */
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
-for($page = 500001;$page <= 597006; $page++)
+for($page = 510000;$page <= 597006; $page++)
 	{
 		$link	=	'https://www.fbise.edu.pk/linkrollno-hssc-11.php?roll_no='.$page;
 		echo 	"$page\n";
